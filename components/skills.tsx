1bcea9e239ffaa097f63523b063691c0104d6e7c
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-09-18 03:43:25 -0300

    add skills component

---
+++ components/skills.tsx
@@ -0,0 +1,48 @@
+'use client'
+
+import React from 'react'
+import { skillsData } from '@/lib/data'
+import { motion } from 'framer-motion'
+import useScrollState from '@/hooks/use-scroll-state'
+import SectionHeading from './section-heading'
+
+export default function Skills() {
+
+  const { ref } = useScrollState({sectionName: "Habilidades", threshold: 0.75})
+  const skillAnimation = {
+    intial: {opacity: 0, scale: 0},
+    animate: (index: number) => ({
+        opacity: 1,
+        scale: 1,
+        transition: {
+            delay: 0.05 * index,
+            type: 'spring',
+            stiffness: 400,
+            damping: 18
+        }
+    }) 
+}  
+
+  return (
+    <section
+    id='skills'
+    className='scroll-mt-28 mt-28 sm:mt-40'
+    ref={ref}>
+        <SectionHeading>Habilidades</SectionHeading>
+            <ul className='flex flex-wrap max-w-[40rem] sm:max-w-[53rem] gap-5 justify-center text-gray-800'>
+                {skillsData.map((skill, index) => (
+                    <motion.li 
+                    key={skill}
+                    className='bg-gray-100 text-lg rounded-xl py-3 px-5 border border-black/10 shadow-md'
+                    variants={skillAnimation}
+                    initial='intial'
+                    whileInView='animate'
+                    viewport={{ once: true }}
+                    custom={index}
+
+                    >{skill}</motion.li>
+                ))}
+            </ul>
+    </section>
+  )
+}
