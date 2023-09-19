ee28d717ed550ca83eec3e9a00cf2d50c43c1e3e
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-09-19 01:15:26 -0300

    section heading style changes

--- components/section-heading.tsx
+++ components/section-heading.tsx
@@ -6,7 +6,7 @@ type SectionHeadingProps = {
 
 export default function SectionHeading( { children }: SectionHeadingProps ) {
   return (
-    <h2 className='text-3xl font-medium capitalize mb-8 text-center'>
+    <h2 className='text-3xl font-medium capitalize mb-8 text-center mt-28 sm:mt-40'>
       { children }
     </h2>
   )
