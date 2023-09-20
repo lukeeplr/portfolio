d11d6dc8f444f3f09b5b567e43701ca602a19f89
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-09-20 00:21:36 -0300

    adds header dark mode

--- components/header.tsx
+++ components/header.tsx
@@ -13,20 +13,21 @@ export default function Header() {
   
     return (
     <header className='z-[999] relative'>
-        <motion.div className='fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full'
+        <motion.div className='fixed top-0 left-1/2 -translate-x-1/2 h-[4.5rem] w-full rounded-none border border-white  border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full
+         dark:border-black dark:border-opacity-10 dark:bg-zinc-900 dark:bg-opacity-80'
         initial={{ y: -100, x: "-50%", opacity: 0}}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
         ></motion.div>
 
         <nav className="fixed flex top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-            <ul className='flex w-[22rem] items-center justify-center flex-wrap gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-2'>
+            <ul className='flex w-[22rem] items-center justify-center flex-wrap gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-2 dark:text-zinc-400'>
                 {links.map( link => (
                     <motion.li className='h-3/4 flex items-center justify-center'
                     key={link.name}
                     initial={{ y: -100, opacity: 0}}
                     animate={{ y: 0, opacity: 1 }}>
-                        <Link className={clsx("relative flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition", {
-                            "text-gray-950": (activeSection === link.name)
+                        <Link className={clsx("relative flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:hover:text-white", {
+                            "text-gray-950 dark:text-white": (activeSection === link.name)
                         })}
                         href={link.hash}
                         onClick={() => {
@@ -36,7 +37,7 @@ export default function Header() {
                         >
                             {link.name}
                             {(activeSection === link.name) && (
-                                <motion.span className="absolute inset-1 rounded-full bg-gray-200/40 -z-10"
+                                <motion.span className="absolute inset-1 rounded-full bg-gray-200/40 -z-10 dark:bg-zinc-700/40"
                                 layoutId='activeSection'
                                 transition={{
                                     type: "spring",
