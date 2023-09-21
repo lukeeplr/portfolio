acd5cc19e2a7c161065b1e943c211291f31a2345
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-09-21 20:20:46 -0300

    section divider dark mode

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion'
 
 export default function SectionDivider() {
   return (
-    <motion.div className='bg-gray-200 h-16 mt-20 -mb-20 w-1 rounded-full hidden sm:block dark:bg-opacity-30'
+    <motion.div className='bg-gray-200 h-16 mt-20 -mb-20 w-1 rounded-full hidden sm:block dark:bg-opacity-20'
     initial={{ y: 150, opacity: 0}}
     animate={{ y: 0, opacity: 1 }}
     transition={{ delay: 0.9 }}  
