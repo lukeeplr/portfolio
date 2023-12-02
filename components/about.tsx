1a6131fe899ce768e972b296968f3615499acf15
Lucas Chaves <53486649+lukeeplr@users.noreply.github.com>
2023-12-02 12:32:45 -0300

    raw: spelling correction

--- components/about.tsx
+++ components/about.tsx
@@ -23,7 +23,7 @@ export default function About () {
         <p className='mb-3 dark:text-white/90'>Me chamo <strong>Lucas</strong>, tenho 22 anos, sou <strong>brasileiro</strong>, nascido em Alagoas, atualmente cursando o 7º perído de <strong>Ciência da Computação</strong> pela <strong>Universidade Federal de Alagoas</strong> e atuo como <strong>Desenvolvedor Front-End</strong>.</p>
         <p className="mb-3 dark:text-white/90">Meu primeiro contato com o mundo do desenvolvimento foi em 2018 com a programação web, trabalhando em uma start-up com foco em jogos online, que me influenciou a seguir o caminho atual. Logo após, em 2019, iniciei minha graduação em Ciência da Computação, expandi meus horizontes e nos dias de hoje estou atuando como Desenvolvedor Front-End autônomo.</p>
         <p className="mb-3 dark:text-white/90">
-        Apaixonado por música, e-sports e design, sou ex-jogador profissional de Counter-Strike e guitarrista nas horas vagas, focado atualmente nos meus projetos pessoas, pelos quais tenho uma carinho gigantesco.
+        Apaixonado por música, e-sports e design, sou ex-jogador profissional de Counter-Strike e guitarrista nas horas vagas, focado atualmente nos meus projetos pessoas, pelos quais tenho um carinho gigantesco.
         </p>
     </motion.section>
   )
